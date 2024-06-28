98b6139730b991e020320f405cf8054fb6030f35
mila mihel <mila_mihel@outlook.com>
2024-06-28 14:26:16 -0600

    Added "Hello, World!" with variable

--- python/first.py
+++ python/first.py
@@ -1,2 +1,6 @@
+# Определяем переменную message и присваиваем ей значение "Hello, World!"
 message = "Hello, World!"
+
+# Выводим значение переменной message на экран
+#Print this is:function
 print(message)
