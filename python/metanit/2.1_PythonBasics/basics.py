21351ae1d1c717ed5efd0014ee9252fe2e0135f3
mila mihel <mila_mihel@outlook.com>
2024-07-13 13:24:55 -0600

    Small Change

--- python/metanit/2.1_PythonBasics/basics.py
+++ python/metanit/2.1_PythonBasics/basics.py
@@ -1,2 +1,15 @@
 print(2+1) 
 print("Hello")
+print("Hello, world!")
+print("Hello, world!")
+slash_string = "Слеш: \\"
+print(slash_string)
+single_quote_string = "Одинарная кавычка: \'"
+print(single_quote_string)
+double_quote_string = "Двойная кавычка: \""
+print(double_quote_string)
+double_quote_string = "Двойная кавычка: \""
+print(double_quote_string)
+new_line_string = "Это первая строка\nЭто вторая строка"
+print(new_line_string)
+
