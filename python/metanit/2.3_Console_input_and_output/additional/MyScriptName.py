cb32207484198550f894407eb08b5ee3fe97cee8
mila mihel <mila_mihel@outlook.com>
2024-07-17 15:39:37 -0600

    user info script

--- python/metanit/2.3_Console_input_and_output/additional/MyScriptName.py
+++ python/metanit/2.3_Console_input_and_output/additional/MyScriptName.py
@@ -1,4 +1,4 @@
-name_of_script = "-=My name script=-"
+name_of_script = "-= My name script =-"
 print(name_of_script)
 
 name = input("enter name: ")
---
+++ python/metanit/2.3_Console_input_and_output/additional/user_info.py
@@ -0,0 +1,7 @@
+name_of_script = "-= user info script =-"
+print(name_of_script)
+
+name = input("enter name: ")
+sName = input("enter sName: ")
+
+print(f"Hello {name} {sName} {12}!")
