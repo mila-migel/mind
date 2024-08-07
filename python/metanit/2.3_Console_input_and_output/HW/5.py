25beaf0d347d1f26fd55bb6b88b111e514366701
mila mihel <mila_mihel@outlook.com>
2024-08-07 13:11:02 -0600

    added Metanit.2

---
+++ python/metanit/2.3_Console_input_and_output/HW/5.py
@@ -0,0 +1,4 @@
+#Создайте переменную is_sunny и присвойте ей логическое значение True. 
+
+is_sunny = True
+print(is_sunny)
---
+++ python/metanit/2.3_Console_input_and_output/HW/6.py
@@ -0,0 +1,6 @@
+#Объявите переменную text и присвойте ей строку "Python" и 
+# вивидете первий символ строки
+
+text = "python"
+first_char = text[0]
+print(first_char)
