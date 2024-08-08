2ca87a5a8ab4faadb1af35c66c60e643c34eda8b
mila mihel <mila_mihel@outlook.com>
2024-08-08 12:46:31 -0600

    Metanit 4

--- python/metanit/2.3_Console_input_and_output/HW/cycle.py/6.py
+++ python/metanit/2.3_Console_input_and_output/HW/cycle.py/6.py
@@ -1,2 +1,11 @@
 #Напишите программу, которая выводит числа Фибоначчи до 100.
 
+i = 2
+previous = 1
+while i <= 80:  
+    a = i
+    i = i + previous
+    print(i) 
+    previous = a
+
+print("end")
