c1d862da3b46742dcc071bb105d76fa334e99fe9
mila mihel <mila_mihel@outlook.com>
2024-08-15 15:47:12 -0600

    Metanit 8

---
+++ python/metanit/2.15_FunctionParameters/CW/2.py
@@ -0,0 +1,6 @@
+# Используйте for для итерации по списку имен и вывода каждого имени.
+
+names = ["Mila", "Vika", "Yaroslav"]
+
+for name in names:
+    print(name)
---
+++ python/metanit/2.15_FunctionParameters/CW/3.py
@@ -0,0 +1,8 @@
+#  Напишите программу, которая считает сумму чисел в списке.
+
+numbers = [1, 2, 3, 4, 5.]
+total = 0
+
+for numbers in numbers:
+    total = numbers
+    print("Сумма чисел в списке:", total)
