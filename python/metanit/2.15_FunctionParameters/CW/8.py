4f79b6225532d777d0d828775d68281cb11d7065
mila mihel <mila_mihel@outlook.com>
2024-08-23 12:19:38 -0600

    metanit 9

--- python/metanit/2.15_FunctionParameters/CW/8.py
+++ python/metanit/2.15_FunctionParameters/CW/8.py
@@ -1,11 +1,13 @@
  # Напишите программу, которая проверяет, содержит ли строка гласные буквы.
 
-vowels = "aeiou"
-text = input("Enter a string: ")
+vowels = "aeiou" # эта строка содержит все гласние буквы на англиском языке
+text = input("Enter a string: ") # запрашивает вивод строки у пользивателя 
 
-contains_vowel = False
+contains_vowel = False # устанавливает флаг которий будет 
+# показывать найдела ли гласная буква в строке
 
-for char in text.lower():
+for char in text.lower(): # цикл приходит по каждому символу в строке text 
+    # преведенной к нижнему регистеру с помощью  text.lover().
     if char in vowels:
         contains_vowel = True
         break
